7d560846e97d5e50457a8b588dea1b5a7a4e5ab6
Chiara Marie Kappe <ckappe@3-H-7.42heilbronn.de>
2026-03-09 20:39:01 +0100

    added WrongAnimal and WrongCat.cpp/.hpp

---
+++ ex00/WrongAnimal.cpp
@@ -0,0 +1,41 @@
+#include "WrongAnimal.hpp"
+
+// -----------------------------------------------------
+// *** CONSTRUCTOR & DESTRUCTOR ***
+// -----------------------------------------------------
+WrongAnimal::WrongAnimal() : type("WrongAnimal")
+{
+	std::cout << "WrongAnimal default constructor called" << std::endl;
+}
+
+WrongAnimal::WrongAnimal(const WrongAnimal& other) : type(other.type)
+{
+	std::cout << "WrongAnimal copy constructor called" << std::endl;
+}
+
+WrongAnimal& WrongAnimal::operator=(const WrongAnimal& other)
+{
+	if (this != &other)
+		type = other.type;
+	std::cout << "WrongAnimal assignment operator called" << std::endl;
+	return *this;
+}
+
+WrongAnimal::~WrongAnimal()
+{
+	std::cout << "WrongAnimal destructor called" << std::endl;
+}
+
+// -----------------------------------------------------
+// *** MEMBER FUNCTIONS ***
+// -----------------------------------------------------
+
+void WrongAnimal::makeSound() const
+{
+	std::cout << "WrongAnimal makes a generic sound" << std::endl;
+}
+
+std::string WrongAnimal::getType() const
+{
+	return type;
+}
